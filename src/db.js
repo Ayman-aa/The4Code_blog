c9df058c186ca115c6f394953b4f0d5db9be0aae
meriamdamoun <damoun.meriam@gmail.com>
2025-02-26 21:23:44 +0100

    db

---
+++ src/db.js
@@ -0,0 +1,42 @@
+export const  articles = [
+    {
+      "id": 1,
+      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/640px-JavaScript-logo.png",
+      "title": "Understanding JavaScript Closures",
+      "description": "This article explains the concept of closures in JavaScript, how they work, and why they are important.",
+      "category": "JavaScript",
+      "hosted_by": "Tech World"
+    },
+    {
+      "id": 2,
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJmII_yCFN8HSvX6Dr5h6t0JXPbudAoM4uzg&s",
+      "title": "CSS Grid Layout: A Beginner's Guide",
+      "description": "Learn the basics of CSS Grid Layout and how to create responsive layouts with minimal effort.",
+      "category": "CSS",
+      "hosted_by": "Web Design Pro"
+    },
+    {
+      "id": 3,
+      "image": "https://img.freepik.com/premium-photo/electronic-circuit-board-abstract-futuristic-echnology-background-with-glowing_7419-380.jpg",
+      "title": "The Rise of Artificial Intelligence",
+      "description": "Explore how AI is transforming industries, from healthcare to finance, and what the future holds.",
+      "category": "Technology",
+      "hosted_by": "Future Tech"
+    },
+    {
+      "id": 4,
+      "image": "https://codegnan.com/wp-content/uploads/2024/03/reactjs-vs-angular-framework-comparison-for-javascript.webp",
+      "title": "React vs Angular: Which is Better for Your Project?",
+      "description": "A comparison between React and Angular frameworks, with pros and cons to help you choose the right one.",
+      "category": "JavaScript Frameworks",
+      "hosted_by": "Dev Insights"
+    },
+    {
+      "id": 5,
+      "image": "https://www.theforage.com/blog/wp-content/uploads/2022/12/what-is-cybersecurity.jpg",
+      "title": "The Importance of Cybersecurity in 2025",
+      "description": "With growing cyber threats, this article discusses the importance of securing your digital presence and how to do it.",
+      "category": "Cybersecurity",
+      "hosted_by": "Secure Tech"
+    }
+  ]
