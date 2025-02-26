42e97b3846351c50151a4f03a55515336dd34e0e
Ayman Aamam <57199947+Ayman-aa@users.noreply.github.com>
2025-02-26 19:25:53 +0000

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import CategoryShowcase from "./components/Category_showcase/CategoryShowcase";
+import CategoryShowcase from "./components/CategoryShowcase/CategoryShowcase";
 import Footer from "./components/Footer/Footer";
 import Newsletter from "./components/Newsletter/Newsletter";
 import Articles from "./components/Articles/Articles";
