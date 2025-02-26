679226cc326db0403d4de953e97c8b0e407031d5
walislorex <soussiwaliss@gmail.com>
2025-02-26 18:40:53 +0000

    posts page and links added to the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Code, Coffee, LogIn, Menu, Search, Sparkles, Star, UserPlus, X, Zap } from "lucide-react";
 import React from "react";
+import { NavLink } from "react-router";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -20,27 +21,12 @@ function Navbar() {
               </span>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-              <a href="#" className="border-orange-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+              <NavLink to="/" className="border-orange-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                 <Sparkles className="w-4 h-4 mr-1" /> Home
-              </a>
-              <a
-                href="#"
-                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-              >
+              </NavLink>
+              <NavLink to="/posts" className="border-orange-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                 <Zap className="w-4 h-4 mr-1" /> Articles
-              </a>
-              <a
-                href="#"
-                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-              >
-                <Star className="w-4 h-4 mr-1" /> Categories
-              </a>
-              <a
-                href="#"
-                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-              >
-                <Coffee className="w-4 h-4 mr-1" /> About
-              </a>
+              </NavLink>
             </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center sm:space-x-4">
@@ -80,27 +66,12 @@ function Navbar() {
       {/* Mobile menu, show/hide based on menu state */}
       <div className={`${isMenuOpen ? "block" : "hidden"} sm:hidden`}>
         <div className="pt-2 pb-3 space-y-1">
-          <a href="#" className="bg-orange-50 border-orange-500 text-orange-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
+          <NavLink to="/" className="bg-orange-50 border-orange-500 text-orange-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
             <Sparkles className="w-4 h-4 inline mr-1" /> Home
-          </a>
-          <a
-            href="#"
-            className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-          >
+          </NavLink>
+          <NavLink to="/posts" className="bg-orange-50 border-orange-500 text-orange-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
             <Zap className="w-4 h-4 inline mr-1" /> Articles
-          </a>
-          <a
-            href="#"
-            className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-          >
-            <Star className="w-4 h-4 inline mr-1" /> Categories
-          </a>
-          <a
-            href="#"
-            className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-          >
-            <Coffee className="w-4 h-4 inline mr-1" /> About
-          </a>
+          </NavLink>
         </div>
         <div className="pt-4 pb-3 border-t border-gray-200">
           <div className="mt-3 px-2 space-y-1">
